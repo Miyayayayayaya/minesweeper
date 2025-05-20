020f204ce00487b652518a2e9d7c2100bbd7edd4
Miyayayayayaya <s1F102502542@iniad.org>
2025-05-20 14:04:40 +0900

    爆弾座標の乱数出力

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,63 @@
 'use client';
 import { useState } from 'react';
 import styles from './page.module.css';
-const down = (n: number) => {
-  console.log(n);
-  if (n === 0) {
-    return;
-  } else {
-    return down(n - 1);
+// const down = (n: number) => {
+//   console.log(n);
+//   if (n === 0) {
+//     return;
+//   } else {
+//     return down(n - 1);
+//   }
+// };
+// down(10);
+// const sum1 = (n: number): number => {
+//   if (n === 0) {
+//     return 0;
+//   } else {
+//     return sum1(n - 1) + n;
+//   }
+// };
+
+// const sum2 = (n: number, m: number): number => {
+//   if (m === n) {
+//     return n;
+//   } else {
+//     return sum2(n, m - 1) + m;
+//   }
+// };
+
+// const sum3 = (n: number, m: number): number => {
+//   return ((n + m) * (m - n + 1)) / 2;
+// };
+
+// console.log(sum3(4, 10));
+
+// console.log(sum2(4, 10));
+
+// console.log(sum1(10));
+
+const randomBombPosition = () => {
+  const positions = new Set<string>();
+  const line: [number, number][] = [];
+  const getRandomBomb = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  for (let i = 0; i < 9; i++) {
+    const x_bombPosition = getRandomBomb(0, 8);
+    const y_bombPosition = getRandomBomb(0, 8);
+    const key = `${x_bombPosition},${y_bombPosition}`;
+    if (!positions.has(key)) {
+      positions.add(key);
+      line.push([x_bombPosition, y_bombPosition]);
+    }
   }
+  return line;
 };
-down(10);
-const sum1 = (n: number): number => {
-  if (n === 0) {
-    return 0;
-  } else {
-    return sum1(n - 1) + n;
-  }
-};
-
-const sum2 = (n: number, m: number): number => {
-  if (m === n) {
-    return n;
-  } else {
-    return sum2(n, m - 1) + m;
-  }
-};
-
-const sum3 = (n: number, m: number): number => {
-  return ((n + m) * (m - n + 1)) / 2;
-};
-
-console.log(sum3(4, 10));
-
-console.log(sum2(4, 10));
-
-console.log(sum1(10));
 
 export default function Home() {
   const [sampleCounter, setSampleCounter] = useState(0);
-  const [numbers, setNumbers] = useState([0, 0, 0, 0, 0]);
+  // const [numbers, setNumbers] = useState([0, 0, 0, 0, 0]);
   const [userInputs, setUserInput] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -50,7 +69,7 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const [bombPlace, setBombPlace] = useState([
+  const [bombMap, setBombMap] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -62,24 +81,32 @@ export default function Home() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-  const clickHandler = () => {
-    setSampleCounter((sampleCounter + 1) % 14); //余り
-    console.log(sampleCounter);
+  const clickHandler = (x: number, y: number) => {
+    const newBombMap = structuredClone(bombMap);
+    const bomb = randomBombPosition();
+    console.log(bomb);
 
-    const newNumbers = structuredClone(numbers);
-    newNumbers[sampleCounter % 5] += 1;
-    setNumbers(newNumbers);
-    console.log(numbers);
-    const calculateTotal = (arr: number[], counter: number) => {
-      let total = 0;
-      for (let i = 0; i < 5; i++) {
-        total += arr[i];
-      }
-      return total + counter;
-    };
-    const total = calculateTotal(numbers, sampleCounter);
-    console.log(total);
+    console.log(x, y);
   };
+
+  // const clickHandler = () => {
+  //   setSampleCounter((sampleCounter + 1) % 14); //余り
+  //   console.log(sampleCounter);
+
+  //   const newNumbers = structuredClone(numbers);
+  //   newNumbers[sampleCounter % 5] += 1;
+  //   setNumbers(newNumbers);
+  //   console.log(numbers);
+  //   const calculateTotal = (arr: number[], counter: number) => {
+  //     let total = 0;
+  //     for (let i = 0; i < 5; i++) {
+  //       total += arr[i];
+  //     }
+  //     return total + counter;
+  //   };
+  //   const total = calculateTotal(numbers, sampleCounter);
+  //   console.log(total);
+  // };
 
   return (
     <div className={styles.container}>
@@ -88,6 +115,8 @@ export default function Home() {
           row.map((color, x) => (
             <div
               className={styles.samplecell}
+              key={`${x}-${y}`}
+              onClick={() => clickHandler(x, y)}
               style={{ backgroundPosition: `${-30 * sampleCounter}px` }}
             />
           )),
