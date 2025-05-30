b5e661833c6f117c104442a8f7301a41026b550e
Miyayayayayaya <s1F102502542@iniad.org>
2025-05-30 22:52:44 +0900

    再帰できない

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,19 +17,6 @@ import styles from './page.module.css';
 //     return sum1(n - 1) + n;
 //   }
 // };
-
-// const sum2 = (n: number, m: number): number => {
-//   if (m === n) {
-//     return n;
-//   } else {
-//     return sum2(n, m - 1) + m;
-//   }
-// };
-
-// const sum3 = (n: number, m: number): number => {
-//   return ((n + m) * (m - n + 1)) / 2;
-// };
-
 // console.log(sum3(4, 10));
 
 // console.log(sum2(4, 10));
@@ -72,10 +59,52 @@ const randomBombPosition = (x: number, y: number) => {
 };
 
 let Maked = true;
+// const sum2 = (n: number, m: number): number => {
+//   if (m === n) {
+//     return n;
+//   } else {
+//     return sum2(n, m - 1) + m;
+//   }
+// };
+// const sum3 = (n: number, m: number): number => {
+//   return ((n + m) * (m - n + 1)) / 2;
+// };
+
+const openCheck = (x: number, y: number, board: number[][]) => {
+  const openBoard: [number, number][] = []; //周りのセルを開けるため
+  // const keepOpenCheck: [number, number][] = []; //==0のセル、中心の座標を記録
+  for (const [dy, dx] of directions) {
+    const ny = y + dy;
+    const nx = x + dx;
+    if (board[ny] === undefined || board[ny][nx] === undefined || board[ny][nx] === 0) {
+      openBoard.push([ny, nx]);
+    }
+  }
+  return openBoard;
+};
+const repeatOpen = (x: number, y: number, openBoard: number[][]) => {
+  const openCheckBoard: [number, number][] = [];
+  const checkNumber = openCheck(x, y, openBoard);
+  for (const [ky, kx] of checkNumber) {
+    for (const [dy, dx] of directions) {
+      const ny = ky + dy;
+      const nx = kx + dx;
+      if (ny >= 0 && ny < 9 && nx >= 0 && nx < 9) {
+        openCheckBoard.push([ny, nx]);
+        console.log('開けるやつ：', openCheckBoard);
+        if (openBoard[ny][nx] === 0) {
+          openBoard[ny][nx] = 1;
+          repeatOpen(nx, ny, openBoard);
+        }
+      }
+    }
+  }
+  return openCheckBoard;
+};
 
 export default function Home() {
-  const [sampleCounter, setSampleCounter] = useState(0);
-  const [numbers, setNumbers] = useState([0, 0, 0, 0, 0]);
+  // const [sampleCounter, setSampleCounter] = useState(0);
+  // const [numbers, setNumbers] = useState([0, 0, 0, 0, 0]);
   const [userInputs, setUserInput] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -140,18 +169,47 @@ export default function Home() {
       }
       setBombMap(newBombMap);
     }
-
-    board[y][x];
+    const newUserInputs = structuredClone(userInputs);
+    newUserInputs[y][x] = 3;
+    setUserInput(newUserInputs);
+    const newBoard = structuredClone(board);
+    newBoard[y][x] = 1;
+    setBoard(newBoard);
+    const openBoard = repeatOpen(x, y, newBoard);
+    for (const [ky, kx] of openBoard) {
+      newBoard[ky][kx] = 1;
+      if (bombMap[ky][kx] === 0) {
+        openCheck(kx, ky, board);
+      }
+    }
+    setBoard(newBoard);
   };
   const handleRightClick = (e: React.MouseEvent, x: number, y: number) => {
     e.preventDefault();
-    console.log('右クリックされた座標:', x, y);
     const newUserInputs = structuredClone(userInputs);
     userInputs[y][x]++;
-    console.log('クリック数:', userInputs[y][x]);
     newUserInputs[y][x] = userInputs[y][x] % 3;
     setUserInput(newUserInputs);
   };
+
+  // const clickHandler = () => {
+  //   setSampleCounter((sampleCounter + 1) % 14); //余り
+  //   console.log(sampleCounter);
+
+  //   const newNumbers = structuredClone(numbers);
+  //   newNumbers[sampleCounter % 5] += 1;
+  //   setNumbers(newNumbers);
+  //   console.log(numbers);
+  //   const calculateTotal = (arr: number[], counter: number) => {
+  //     let total = 0;
+  //     for (let i = 0; i < 5; i++) {
+  //       total += arr[i];
+  //     }
+  //     return total + counter;
+  //   };
+  //   const total = calculateTotal(numbers, sampleCounter);
+  //   console.log(total);
+  // };
 
   return (
     <div className={styles.container}>
@@ -165,7 +223,12 @@ export default function Home() {
               onContextMenu={(e) => handleRightClick(e, x, y)}
               style={{ backgroundPosition: `${-30 * (bombMap[y][x] - 1)}px` }}
             >
-              <div className={styles.boardCell}>
+              <div
+                className={styles.boardCell}
+                style={{
+                  backgroundPosition: userInputs[y][x] === 0 ? undefined : `-30px`,
+                }}
+              >
                 <div
                   className={styles.userInputsCell}
                   style={{
@@ -174,7 +237,7 @@ export default function Home() {
                         ? `-270px`
                         : userInputs[y][x] === 2
                           ? `-240px`
-                          : undefined,
+                          : `30px`,
                   }}
                 >
                   {bombMap[y][x]}
